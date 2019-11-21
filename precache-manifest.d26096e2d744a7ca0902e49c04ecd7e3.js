cec4743fae886e87d768f5f9ec1cb8b675d9a14a
Cosmin Andrei <cosminandrei.cnt@gmail.com>
2019-11-21 11:30:29 +0200

    Updates

---
+++ precache-manifest.d26096e2d744a7ca0902e49c04ecd7e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee57ee3d0a4ec6f0dfe60180165300e8",
+    "url": "/local-storage/index.html"
+  },
+  {
+    "revision": "48b1069682b0fb91d9ef",
+    "url": "/local-storage/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "77feb1894c56c9158e94",
+    "url": "/local-storage/static/js/2.f391353a.chunk.js"
+  },
+  {
+    "revision": "48b1069682b0fb91d9ef",
+    "url": "/local-storage/static/js/main.65cfd8e6.chunk.js"
+  },
+  {
+    "revision": "eb1a72ebb37a4e49a47e",
+    "url": "/local-storage/static/js/runtime-main.3e53ad12.js"
+  }
+]);
