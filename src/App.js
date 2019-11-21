e777ac9dcdb0e3fa7b53a88f4c4aa61bc7cb2a29
Cosmin Andrei <cosminandrei.cnt@gmail.com>
2019-11-21 11:25:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SAMPLE_JSON = {
+  "one": 1,
+  "two": true,
+  "3": "three"
 }
+ 
+const LOCALSTORAGE_KEY = 'someJson'
+ 
+export default class App extends React.Component {
+	constructor (props) {
+  	super(props)
+    
+    this.json = {}
+  }
+  
+  componentWillMount () {
+    this.loadJson()
+  }
+ 
+  validateJson (json) {
+    let validJson
+    
+    try{
+      validJson = JSON.stringify(JSON.parse(this.state.json), null, 2)
+    } catch(e) {
+      throw e
+    }
+    
+    return validJson
+  }
+  
+  loadJson = () => {
+    const json = window.localStorage.getItem(LOCALSTORAGE_KEY) || JSON.stringify(SAMPLE_JSON, null, 2)
+    this.setState({ json })
+  }
+  
+  saveJson = () => {
+    const validJson = this.validateJson(this.state.json)
+    
+    if (!validJson) return;
+    
+    window.localStorage.setItem(
+      LOCALSTORAGE_KEY,
+      validJson
+    )
+  }
+  
+  handleChange = e => this.setState({
+    json: e.target.value
+  })
 
-export default App;
+  
+  
+  render () {
+    return (
+      <div style={{left: "50%", position: "absolute", width:"70%" }}>
+        <textarea onChange={this.handleChange} value={this.state.json} style={{height:"30vh", width:"30%"}}/>
+        <br/>
+        <button onClick={this.saveJson}>SAVE to LocalStorage</button> 
+        <button onClick={this.loadJson}>LOAD from LocalStorage</button>
+<p>save/load JSON data via a React Component</p>
+      </div>
+    )
+  }
+}
